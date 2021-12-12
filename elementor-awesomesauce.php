95f5e111e7c0dc6da306b4b65379a284469340f3
renn72 <simplesam8856@gmail.com>
2021-12-13 08:26:59 +1100

    .

---
+++ elementor-awesomesauce.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Elementor Awesomesauce WordPress Plugin
+ *
+ * @package ElementorAwesomesauce
+ *
+ * Plugin Name: Elementor Awesomesauce
+ * Description: Simple Elementor plugin example
+ * Plugin URI:  https://www.david-warner.dev
+ * Version:     1.0.0
+ * Author:      David Warner
+ * Author URI:  https://www.david-warner.dev
+ * Text Domain: elementor-awesomesauce
+ */
+
+define( 'ELEMENTOR_AWESOMESAUCE', __FILE__ );
+
+/**
+ * Include the Elementor_Awesomesauce class.
+ */
+require plugin_dir_path( ELEMENTOR_AWESOMESAUCE ) . 'class-elementor-awesomesauce.php';
